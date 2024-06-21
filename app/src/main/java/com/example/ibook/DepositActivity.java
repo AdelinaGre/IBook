f984e472ff2f6b4bf209fbc65e87e77cb6ff3516
adela <adelinagrebencea@gmail.com>
2024-06-21 20:05:50 +0300

    categories page

--- app/src/main/java/com/example/ibook/DepositActivity.java
+++ app/src/main/java/com/example/ibook/DepositActivity.java
@@ -1,6 +1,9 @@
 package com.example.ibook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,24 @@ public class DepositActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_deposit);
+
+        // Set padding based on system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Assuming backBtn is your ImageView or ImageButton
+        ImageView backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to DetailBookActivity
+                Intent intent = new Intent(DepositActivity.this, DetailBookActivity.class);
+                startActivity(intent);
+                finish(); // Optional: Call finish() if you want to close DepositActivity after navigating
+            }
         });
     }
 }
